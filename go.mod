38d653ee323519ed14d8c6528fdabf7fe5eb4f9a
miyamoen <akihito.biblioteca@gmail.com>
2019-05-19 13:36:36 +0900

    ping pong

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/miyamoen/try-go
+
+go 1.12
+
+require github.com/bwmarrin/discordgo v0.19.0
--- main.go
+++ main.go
@@ -2,12 +2,57 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	token = ""
 )
 
 func main() {
-	var message = "Hello, 世界"
-	fmt.Println(message)
-	fmt.Println("hello world")
-	fmt.Println(1 + 5.4)
-	println("ddd")
+	discord, err := discordgo.New("Bot " + token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	discord.AddHandler(onMessageCreate)
+	err = discord.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	defer discord.Close()
+
+	return
+}
+
+func onMessageCreate(session *discordgo.Session, msg *discordgo.MessageCreate) {
+	user := msg.Author
+	if user.ID == session.State.User.ID || user.Bot {
+		return
+	}
+
+	content := msg.Content
+
+	switch content {
+	case "ping", "Ping":
+		session.ChannelMessageSend(msg.ChannelID, "Pong!")
+
+	case "pong", "Pong!":
+		session.ChannelMessageSend(msg.ChannelID, "Ping!")
+	}
+
+	fmt.Printf("Message: %+v\n", msg.Message)
 }
